58a25c329393b15105d6719e04368f97a6856ac5
Sergei Lebedev <s@slebe.dev>
2026-08-27 20:56:11 +0200

    Test a working repo

--- service/tests/unittests/corelib/hgprocess_test.cpp
+++ service/tests/unittests/corelib/hgprocess_test.cpp
@@ -1,5 +1,6 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include <spdlog/spdlog.h>
 
 #include "corelib/src/patch/hgprocess.hpp"
 #include "utils/corraleventlooptraits.hpp"
@@ -57,6 +58,85 @@ TEST(HgProcessTest, RunCommandHelp) {
     EXPECT_EQ(result.requiredInputSize, 0);
     co_await process->shutdown();
     delete process;
+  });
+  EXPECT_TRUE(taskExecuted);
+}
+
+TEST(HgProcessTest, NormalRepoSummary) {
+  // Let's create a repo in a temporary folder and then run the summary command
+  // on it.
+  auto l = new uv_loop_t();
+  auto result = uv_loop_init(l);
+  EXPECT_EQ(result, 0);
+  auto loop = LoopPtr(l, deleteLoop);
+  auto process = new HgProcess(loop.get());
+  bool taskExecuted = false;
+
+  corral::run(*loop, [&loop, process, &taskExecuted]() -> corral::Task<void> {
+    char tmpDir[1024];
+    size_t tmpDirSize = sizeof(tmpDir);
+    [&tmpDir, &tmpDirSize]() {
+      ASSERT_EQ(uv_os_tmpdir(tmpDir, &tmpDirSize), 0)
+          << "Failed to get temporary directory";
+    }();
+    auto tempRepoTemplate = std::string(tmpDir) + "/temp_repo_XXXXXX";
+    uv_fs_t req;
+    using CBPType = corral::CBPortal<uv_fs_t *>;
+    CBPType cbp;
+    auto r = co_await corral::untilCBCalled(
+        [&](CBPType::Callback &cb) {
+          req.data = &cb;
+          uv_fs_mkdtemp(
+              loop.get(), &req, tempRepoTemplate.c_str(),
+              +[](uv_fs_t *r) { (*(CBPType::Callback *)r->data)(r); });
+        },
+        cbp);
+    // Otherwise ASSERT_* will not work inside a coroutine.
+    [&r]() {
+      ASSERT_GE(r->result, 0) << "Failed to create temporary folder for repo";
+    }();
+
+    auto tempRepoPath = std::string(r->path);
+    uv_fs_req_cleanup(&req);
+    spdlog::info("Created temporary repo folder: {}", tempRepoPath);
+
+    // Now init repo
+    auto result = co_await process->runCommand({"init", tempRepoPath});
+    [&result]() {
+      ASSERT_EQ(result.resultCode, 0) << "Failed to init repo";
+      EXPECT_FALSE(result.requiresInput);
+      EXPECT_EQ(result.requiredInputSize, 0);
+      EXPECT_THAT(result.error, IsEmpty());
+    }();
+
+    // Run summary
+    result =
+        co_await process->runCommand({"summary", "--repository", tempRepoPath});
+    [&result]() {
+      ASSERT_EQ(result.resultCode, 0) << "Failed to run summary";
+      EXPECT_FALSE(result.requiresInput);
+      EXPECT_EQ(result.requiredInputSize, 0);
+      EXPECT_THAT(result.error, IsEmpty());
+    }();
+
+    taskExecuted = true;
+    co_await process->shutdown();
+    delete process;
+
+    // Cleanup temporary folder
+    uv_fs_t cleanupReq;
+    r = co_await corral::untilCBCalled(
+        [&](CBPType::Callback &cb) {
+          cleanupReq.data = &cb;
+          uv_fs_rmdir(
+              loop.get(), &cleanupReq, tempRepoPath.c_str(),
+              +[](uv_fs_t *r) { (*(CBPType::Callback *)r->data)(r); });
+        },
+        cbp);
+    [&r]() {
+      ASSERT_GE(r->result, 0) << "Failed to cleanup temporary repo folder";
+    }();
+    uv_fs_req_cleanup(&cleanupReq);
   });
   EXPECT_TRUE(taskExecuted);
 }
